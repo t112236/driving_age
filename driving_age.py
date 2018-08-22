2fe4b13ff7b34597493c2aeef2a1dde00a3d25d5
WEN <t112236@gmail.com>
2018-08-22 16:12:15 +0800

    Taiwan & USA

--- driving_age.py
+++ driving_age.py
@@ -1,10 +1,16 @@
-age = input('請輸入年齡: ')
-country = input('請輸入國籍: ')
+age = input('請輸入年齡(age): ')
+country = input('請輸入國籍(country): ')
 age = int(age)
-if country == '台灣':
-	if age > 18:
-		print('你可以考駕照了')
-	else:
-		print('你還不能考駕照')
+if country == '台灣' or country == '美國':
+	if country == '台灣':
+		if age > 18:
+			print('你可以考駕照了')
+		else:
+			print('你還不能考駕照')
+	if country == '美國':
+		if age > 16:
+			print('你可以考駕照了')
+		else:
+			print('未到達法定年齡')
 else:
 	print('請諮詢您的國家 謝謝!')
